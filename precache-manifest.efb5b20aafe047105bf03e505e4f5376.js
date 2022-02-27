589fef0d672a42af908cedfa7a0567555ceb9966
DESKTOP-QERIBPS\Jorge <obrequel337@gmail.com>
2022-02-27 09:46:24 -0300

    Updates

---
+++ precache-manifest.efb5b20aafe047105bf03e505e4f5376.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e56290dd080d399d1cf0",
+    "url": "/designlab-react/static/css/main.a4458905.chunk.css"
+  },
+  {
+    "revision": "e56290dd080d399d1cf0",
+    "url": "/designlab-react/static/js/main.e56290dd.chunk.js"
+  },
+  {
+    "revision": "935b0251a626769e024b",
+    "url": "/designlab-react/static/js/1.935b0251.chunk.js"
+  },
+  {
+    "revision": "45de306f3f336216fdf0",
+    "url": "/designlab-react/static/js/2.45de306f.chunk.js"
+  },
+  {
+    "revision": "56ddf6e9ae4c5b2f62bb",
+    "url": "/designlab-react/static/js/runtime~main.56ddf6e9.js"
+  },
+  {
+    "revision": "3cbe0776caaac869ce67d9bbff234586",
+    "url": "/designlab-react/index.html"
+  }
+];
